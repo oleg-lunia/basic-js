13757e90b46e9e949a2f88cbf2b18b787460b472
Oleg Lunia <oleglunja2@gmail.com>
2021-03-25 01:08:55 +0300

    refactoring: changed the file src/dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let stack = [];
+
+  if (!Array.isArray(members)) {
+    return false;
+  }
+  for (let i = 0; i < members.length; i++) {
+    if (typeof(members[i]) == 'string') {
+      let name = members[i].match(/\b\p{L}/giu);
+    stack.push(name[0].toUpperCase());
+    }
+  }
+  stack.sort();
+  let str = stack.join('');
+  return str;
 };
