966e4c57753bbd9262eeda09e45b9e92f9ed28ac
Oleg Lunia <oleglunja2@gmail.com>
2021-03-24 23:10:18 +0300

    refactoring: changed the file src/carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,19 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof(sampleActivity) != 'string') {
+    return false;
+  }
+  let numSample = Number(sampleActivity);
+  if (isNaN(numSample) == true) {
+    return false;
+  } else if (numSample == 0) {
+    return false;
+  } else if (numSample > 15 || numSample < 0) {
+    return false;
+  } else {
+    let number = Math.log(MODERN_ACTIVITY/numSample)/(Math.log(2)/HALF_LIFE_PERIOD);
+    return Math.ceil(number);
+  }
 };
